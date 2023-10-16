2d6fe4d539cacdc3a5576f08842a466bb6cba276
NiLoBol <sathaporn733@gmail.com>
2023-10-16 15:06:07 +0700

    edit bug

--- src/app/testapp/page.js
+++ src/app/testapp/page.js
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
 async function page() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/todo`)
-      .then((response) => response.json())
-      .then((jsonData) => {
-        console.log(jsonData);
-        return jsonData;
-      })
-      .catch((error) => {
-        console.error('เกิดข้อผิดพลาดในการดึงข้อมูล:', error);
-      });
+    .then((response) => response.json())
+    .then((jsonData) => {
+      console.log(jsonData);
+      return jsonData;
+    })
+    .catch((error) => {
+      console.error("เกิดข้อผิดพลาดในการดึงข้อมูล:", error);
+    });
   console.log(res);
   return (
     <div>
-    {res.Todos.map((data, index) => (
-      <div key={index} className='flex flex-row gap-5 p-10 bg-yellow-200'>
-        <input type="checkbox" checked={data.clickbox} />
-        <p className='text-center text-4xl'>{data.title}</p>
-      </div>
-    ))}
-  </div>
-  
-  )
+      {res.Todos.map((data, index) => (
+        <div key={index} className="flex flex-row gap-5 p-10 bg-yellow-200">
+          <input type="checkbox" checked={data.clickbox} />
+          <p className="text-center text-4xl">{data.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default page
+export default page;
