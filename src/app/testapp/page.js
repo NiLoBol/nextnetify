ad9684b8d3d6391c7042406a00c9907c61bbfff5
NiLoBol <sathaporn733@gmail.com>
2023-10-16 15:30:27 +0700

    edit bug

--- src/app/testapp/page.js
+++ src/app/testapp/page.js
@@ -1,30 +1,30 @@
 import React from "react";
 
-const getTodo = async () => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/todo`, {
-      cache: "no-store",
-    });
+// const getTodo = async () => {
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/todo`, {
+//       cache: "no-store",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch todo");
-    }
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch todo");
+//     }
 
-    return res.json();
-  } catch (error) {
-    console.log("Error loading todo: ", error);
-  }
-};
+//     return res.json();
+//   } catch (error) {
+//     console.log("Error loading todo: ", error);
+//   }
+// };
 
 async function page() {
-  const {Todos} = await getTodo();
+  // const {Todos} = await getTodo();
   return (
     <div>
-      {Todos.map((data, index) => (
+      {/* {Todos.map((data, index) => (
         <div key={index} className="flex flex-row gap-5 p-10 bg-yellow-200">
           <p className="text-center text-4xl">{data.title}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
