67bf4e09b9af1f109aa2b259e0ca6e579a1dbfea
NiLoBol <sathaporn733@gmail.com>
2023-10-16 15:12:36 +0700

    delete input test deploy

--- src/app/testapp/page.js
+++ src/app/testapp/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 async function page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/todo`)
+  const {Todos} = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/todo`)
     .then((response) => response.json())
     .then((jsonData) => {
       return jsonData;
@@ -11,9 +11,9 @@ async function page() {
     });
   return (
     <div>
-      {res.Todos.map((data, index) => (
+      {Todos.map((data, index) => (
         <div key={index} className="flex flex-row gap-5 p-10 bg-yellow-200">
-          <input type="checkbox" checked={data.clickbox} />
+          
           <p className="text-center text-4xl">{data.title}</p>
         </div>
       ))}
